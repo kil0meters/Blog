5e4dda63146602260fdcf17b2a305736b512a0f2
kilometers <kilo@meters.sh>
2023-09-29 11:25:17 -0700

    update

--- build.sh
+++ build.sh
@@ -4,9 +4,3 @@ rm -rf dist public
 zola build
 minify -r -a -o dist/ public/
 cp -n -r public/* dist
-
-for file in $(find ./dist -name '*.xml' -or -name '*.css' -or -name '*.js' -or -name '*.html' -or -name '*.pdf')
-do
-    brotli -Z "$file"
-    gzip --keep --best "$file"
-done
